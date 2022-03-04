c3aba837e5c704825c54b0f9e48f51e9816dca91
ZhangNan <47030616+NanZhang1991@users.noreply.github.com>
2022-03-04 15:05:42 +0800

    Update exception.py

--- exception.py
+++ exception.py
@@ -1,5 +1,8 @@
-     try:
-          error
-     except Exception as e:
-        error_line = e.__traceback__.tb_lineno
-        traceback.format_exc()
+import logging
+logger = logging.getLogger(__name__)
+
+try:
+     error
+except Exception as e:
+   error_line = e.__traceback__.tb_lineno
+   logger.error(traceback.format_exc())
